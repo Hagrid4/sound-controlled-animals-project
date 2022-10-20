bfeeead29d2cb45d248976ed18e5de382d8e9488
Hagrid4 <88029651+Hagrid4@users.noreply.github.com>
2022-10-20 14:02:48 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function startClassification()
 {
     navigator.mediaDevices.getUserMedia({audio: true});
-    classifier = ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/Uh67QPvQy/model.json", modelReady);
+    classifier = ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/lkio1KThV/model.json", modelReady);
 }
 function modelReady()
 {
@@ -16,5 +16,40 @@ function gotResults(error, results)
     else
     {
         console.log('got results')
+
+        random_number_r = Math.floor(Math.random() * 255) + 1;
+        random_number_g = Math.floor(Math.random() * 255) + 1;
+        random_number_b = Math.floor(Math.random() * 255) + 1;
+
+        document.getElementById("cat_detected").innerHTML = cat;
+        document.getElementById("dog_detected").innerHTML = dog;
+
+        document.getElementById("result_label").innerHTML = 'Detected Voice is of - ' + results[0].label;
+
+        document.getElementById("result_label").style.color = "rgb("+random_number_r+", "+random_number_g+", "+random_number_b+")";
+        document.getElementById("cat_detected").style.color = "rgb("+random_number_r+", "+random_number_g+", "+random_number_b+")";
+        document.getElementById("dog_detected").style.color = "rgb("+random_number_r+", "+random_number_g+", "+random_number_b+")";
+
+        img = document.getElementById("hear_img");
+
+        if(results[0].label == 'Dog')
+        {
+            img.src = 'dog.jpg';
+            dog = dog + 1;
+            document.getElementById("dog_detected").innerHTML = dog;
+        }
+        else if(results[0].label == 'Cat')
+        {
+            img.src = 'cat.webp';
+            cat = cat + 1;
+            document.getElementById("cat_detected").innerHTML = cat;
+        }
+        else
+        {
+            img.src = 'Hear_IMG_1.png';
+            document.getElementById("result_label").innerHTML = "Detected Voice is of - Background Noise";
+        }
     }
 }
+cat = 0;
+dog = 0;
